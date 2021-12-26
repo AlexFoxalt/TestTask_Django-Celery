0d9a29a8724ffecf8252db607cff0e0ff2758633
AlexFoxalt <alexfoxalt@gmail.com>
2021-12-26 20:17:58 +0200

    [1.05]

--- CSVFaker/settings.py
+++ CSVFaker/settings.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -60,9 +59,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 
